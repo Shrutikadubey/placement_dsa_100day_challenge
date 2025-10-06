598897a1ff8fe5e6f58352845066d1ca672d569a
Shrutikadubey <113886718+Shrutikadubey@users.noreply.github.com>
2025-10-06 16:54:10 +0000

    day6

---
+++ 01.arrays/1.Easy/6.Rotate_array_left&right_by_k_places.cpp
@@ -0,0 +1,10 @@
+Rotate Array Clockwise
+Difficulty: EasyAccuracy: 51.88%Submissions: 3K+Points: 2
+You are given an array arr[] of integers and an integer k. Your task is to rotate the array k times in the clockwise direction.
+In a single clockwise rotation, the last element of the array moves to the front, and all other elements shift one position to the right.
+
+Examples:
+
+Input: arr[] = [1, 2, 3, 4, 5, 6], k = 2
+Output: [5, 6, 1, 2, 3, 4]
+Explanation: Rotating the array 2 times in clockwise gives the array [5, 6, 1, 2, 3, 4].
---
+++ 01.arrays/1.Easy/8.linear_search.cpp
@@ -0,0 +1,25 @@
+/*Array Search
+Difficulty: BasicAccuracy: 40.95%Submissions: 489K+Points: 1Average Time: 15m
+Given an array, arr[] of n integers, and an integer element x, find whether element x is present in the array. Return the index of the first occurrence of x in the array, or -1 if it doesn't exist.
+
+Examples:
+
+Input: arr[] = [1, 2, 3, 4], x = 3
+Output: 2
+Explanation: For array [1, 2, 3, 4], the element to be searched is 3. Since 3 is present at index 2, the output is 2.*/
+
+
+// Explanation
+
+// Loop through the array arr using an index i.
+// If arr[i] == x, return i immediately (first occurrence found).
+// If the loop finishes without finding x, return -1.
+
+//code
+for (int i = 0; i < arr.size(); i++) {
+            if (arr[i] == x) {
+                return i; // return index of first occurrence
+            }
+        }
+        return -1; // element not found
+    }
